f44001b4f98744265c6485592970b40890896590
simeonuba <101792434+simeonuba@users.noreply.github.com>
2022-06-29 17:15:15 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97,123): 
-        print("{}".format(chr(alphabet)), end="")
+        
+for alpha in range(97, 123):
+    print("{}".format(chr(alpha)), end="")

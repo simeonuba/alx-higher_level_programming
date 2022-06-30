884b5eca69ed326c7492c058c0f9d0ad14d2caa5
simeonuba <101792434+simeonuba@users.noreply.github.com>
2022-06-30 01:45:59 +0100

    python is good

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 for number in range(0, 100):
-   if n == 99:
-        print("{}".format(n))
+    if number == 99:
+        print("{}".format(number))
     else:
-   print("{:02}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
